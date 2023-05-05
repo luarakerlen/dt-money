5013ce66f8af4a7f049148037d3dd5ebbeca9233
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-05-05 00:51:05 -0300

    feat: create modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -12,13 +13,7 @@ export function Header() {
 					<Dialog.Trigger asChild>
 						<NewTransactionButton>Nova transação</NewTransactionButton>
 					</Dialog.Trigger>
-					<Dialog.Portal>
-						<Dialog.Overlay />
-						<Dialog.Content>
-							<Dialog.Title>Nova transação</Dialog.Title>
-							<Dialog.Close />
-						</Dialog.Content>
-					</Dialog.Portal>
+					<NewTransactionModal />
 				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>

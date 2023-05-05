8fc0aa207832351f6cdb2e97c15b79f0693ef5ec
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-05-04 23:54:50 -0300

    feat: create table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,40 @@
 import { Header, Summary } from '../../components';
+import {
+	PriceHighlight,
+	TransactionsContainer,
+	TransactionsTable,
+} from './styles';
 
 export function Transactions() {
 	return (
 		<div>
 			<Header />
 			<Summary />
+
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width='50%'>Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant='income'> R$ 12.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>13/12/2022</td>
+						</tr>
+						<tr>
+							<td width='50%'>Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant='outcome'>
+									- R$ 12.000,00
+								</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>13/12/2022</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
 		</div>
 	);
 }

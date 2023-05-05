1c1f64b70e8ec58a6496dcbff550cc761389d049
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-05-05 00:32:13 -0300

    chore: install radix and add components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -7,7 +8,18 @@ export function Header() {
 		<HeaderContainer>
 			<HeaderContent>
 				<img src={logoImg} alt='logo dt money' />
-				<NewTransactionButton>Nova transação</NewTransactionButton>
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<NewTransactionButton>Nova transação</NewTransactionButton>
+					</Dialog.Trigger>
+					<Dialog.Portal>
+						<Dialog.Overlay />
+						<Dialog.Content>
+							<Dialog.Title>Nova transação</Dialog.Title>
+							<Dialog.Close />
+						</Dialog.Content>
+					</Dialog.Portal>
+				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>
 	);

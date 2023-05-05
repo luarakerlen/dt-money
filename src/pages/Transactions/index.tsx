331a95ba740ccae750137e77fb4cb24269b1ccb2
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-05-05 00:08:17 -0300

    feat: create and add search form component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header, Summary } from '../../components';
+import { SearchForm } from './components';
 import {
 	PriceHighlight,
 	TransactionsContainer,
@@ -12,6 +13,7 @@ export function Transactions() {
 			<Summary />
 
 			<TransactionsContainer>
+				<SearchForm />
 				<TransactionsTable>
 					<tbody>
 						<tr>

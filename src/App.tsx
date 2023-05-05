a2c561e284b2af6bdaafaf5a070175651b48a75c
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-05-04 22:12:48 -0300

    chore: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
-	return <>Hello world!</>;
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<h1>Hello world!</h1>
+		</ThemeProvider>
+	);
 }

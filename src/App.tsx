e25b4507cf0ce7f42b040cdd16efb6ca7a88bdd5
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-05-04 23:01:55 -0300

    chore: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
+import { Transactions } from './pages/index';
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<GlobalStyle />
-			<h1>Hello world!</h1>
+			<Transactions />
 		</ThemeProvider>
 	);
 }
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components/index";
+
+export function Transactions() {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+}

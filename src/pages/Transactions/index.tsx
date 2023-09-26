309a1f7170d7324d614ee1f7730000ecded97300
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-26 17:32:41 -0300

    chore: use context selector hook

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContextSelector } from 'use-context-selector';
 import { Header, Summary } from '../../components';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { dateFormatter, priceFormatter } from '../../utils/formatter';
@@ -10,7 +10,9 @@ import {
 } from './styles';
 
 export function Transactions() {
-	const { transactions } = useContext(TransactionsContext);
+	const transactions = useContextSelector(TransactionsContext, (context) => {
+		return context.transactions;
+	});
 
 	return (
 		<div>

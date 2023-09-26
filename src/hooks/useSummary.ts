adb99c48817ca5115a00fab87651c35a4e7d4c67
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-26 18:49:53 -0300

    use useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useContextSelector } from 'use-context-selector';
 import { TransactionsContext } from '../contexts/TransactionsContext';
 
@@ -7,24 +8,26 @@ export function useSummary() {
 	});
 
 	// { income: 0, outcome: 0, total: 0 }
-	const summary = transactions.reduce(
-		(accumulator, transaction) => {
-			if (transaction.type === 'income') {
-				accumulator.income += transaction.price;
-				accumulator.total += transaction.price;
-			} else {
-				accumulator.outcome += transaction.price;
-				accumulator.total -= transaction.price;
-			}
+	const summary = useMemo(() => {
+		return transactions.reduce(
+			(accumulator, transaction) => {
+				if (transaction.type === 'income') {
+					accumulator.income += transaction.price;
+					accumulator.total += transaction.price;
+				} else {
+					accumulator.outcome += transaction.price;
+					accumulator.total -= transaction.price;
+				}
 
-			return accumulator;
-		},
-		{
-			income: 0,
-			outcome: 0,
-			total: 0,
-		}
-	);
+				return accumulator;
+			},
+			{
+				income: 0,
+				outcome: 0,
+				total: 0,
+			}
+		);
+	}, [transactions]);
 
 	return summary;
 }
